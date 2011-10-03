0aa24c2e9b949e2d4af25a061dde00d9a8fc05ca
Ramon Navarro Bosch <ramon.nb@gmail.com>
2011-10-03 22:45:09 +0200

    Changes at MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.0-dev'
+version = '0.1a1'
 
 setup(name='plone.app.multilingual',
       version=version,
-      description="",
+      description="Multilingual plone ui package, it allow to maintain translation on Dexterity types and Archetypes",
       long_description="""\
+Multilingual Plone UI package, in alpha stage, don't use in production
 """,
       classifiers=[
+        "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       url='http://svn.plone.org/svn/plone/plone.app.multilingual',
       license='GPL',
+      author='Ramon Navarro, Victor Fernanez, awello et al',
+      author_email='r.navarro@iskra.cat',
       packages=find_packages('src'),
       package_dir={'': 'src'},
       namespace_packages=['plone','plone.app'],
