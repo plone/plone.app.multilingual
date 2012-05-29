c9984c67840bc86c2a8e884bd3976384b7b9bc6e
Patrick Gerken <do3cc@patrick-gerken.de>
2012-05-29 22:54:48 +0200

    Reindent

--- src/plone/app/multilingual/browser/javascript/loadContentToTranslateFrom.js
+++ src/plone/app/multilingual/browser/javascript/loadContentToTranslateFrom.js
@@ -1,21 +1,22 @@
-
-
 function reload_height() {
   orig = $('#frame-content .field');
   desti = $('#form-target .field');
   order = 1;
-  $.each(orig, function(index, value) { 
-    if ($(value).height()>$(desti[index]).height()) {
+  $.each(orig, function (index, value) {
+    if ($(value).height() > $(desti[index]).height()) {
       $(desti[index]).height($(value).height());
     } else {
       $(value).height($(desti[index]).height());
     }
-    if (($(value).find('.richtext-field').length>0) || ($(value).find('.textline-field').length>0) || ($(value).find('.localstatic-field').length>0) || ($(value).find('.ArchetypesField-TextField').length>0)) {
-      $(value).prepend("<div class='translator-widget' id='item_translation_"+order+"'></div>");
-      $(value).children('.translator-widget').click(function() {
+    if (($(value).find('.richtext-field').length > 0) || ($(value).find('.textline-field').length > 0) || ($(value).find('.localstatic-field').length > 0) || ($(value).find('.ArchetypesField-TextField').length > 0)) {
+      $(value).prepend("<div class='translator-widget' id='item_translation_" + order + "'></div>");
+      $(value).children('.translator-widget').click(function () {
         var langSource = $('#frame-content #view_language')[0].innerHTML;
         var field = $(value).attr("id"); // Fetch source of text to translate.
-        var jsondata = {'field':field,'lang_source':langSource};
+        var jsondata = {
+          'field': field,
+          'lang_source': langSource
+        };
         var url_translate = $('input#url_translate').val();
 
         is_tiny = false;
@@ -24,7 +25,7 @@ function reload_height() {
           var targetelement = $(desti[index]).find('input');
         } else if ($(desti[index]).find('textarea.mce_editable').length) {
           // mceEditor
-          var editor=$(desti[index]).find('textarea.mce_editable').attr('id');
+          var editor = $(desti[index]).find('textarea.mce_editable').attr('id');
           var is_tiny = true;
         } else if ($(desti[index]).find('textarea').length) {
           // Textarea field
@@ -32,11 +33,11 @@ function reload_height() {
         }
         // Now we call the data
         $.ajax({
-          url: url_translate+'/gtranslation_service',
+          url: url_translate + '/gtranslation_service',
           data: jsondata,
           dataType: 'json',
           type: 'POST',
-          success: function(data){
+          success: function (data) {
             // console.log(data);
             text_target = data.data.translations[0].translatedText;
             console.log(text_target);
@@ -54,29 +55,29 @@ function reload_height() {
   });
 }
 
-jq(document).ready(function() { 
+jq(document).ready(function () {
 
   /* alert about language independent field */
-  $('.languageindependent').click(function() {
+  $('.languageindependent').click(function () {
     alert('Is a languageindependent!');
-    $(this).css('opacity','1');
+    $(this).css('opacity', '1');
   });
 
   /* change the language trigger */
-  jq('#lang-select').change(function() {
-    var url = jq('#lang-select option:selected').val(); 
-    jq('#frame-content').load(url, function() {
+  jq('#lang-select').change(function () {
+    var url = jq('#lang-select option:selected').val();
+    jq('#frame-content').load(url, function () {
       reload_height();
     });
-  }); 
+  });
 
   /* header height */
-  $('#trans-selector').height($('#header-translation').height()+13+$('.formTabs').height());
+  $('#trans-selector').height($('#header-translation').height() + 13 + $('.formTabs').height());
 
   /* select a field on both sides and change the color */
   var babel_selectec = null;
   var orig_babel_select = null;
-  jq('#babel-edit #fieldset-default .field').click(function() {
+  jq('#babel-edit #fieldset-default .field').click(function () {
     if (babel_selectec) {
       $(babel_selectec).toggleClass("selected");
       $(orig_babel_select).toggleClass("selected");
@@ -92,11 +93,9 @@ jq(document).ready(function() {
 
 });
 
-$(window).load(function() {
-  /* initial load the default language left side */
+$(window).load(function () { /* initial load the default language left side */
   var url = jq('#lang-select option:selected').val();
-  jq('#frame-content').load(url, function() {
-      reload_height();
+  jq('#frame-content').load(url, function () {
+    reload_height();
   });
 });
-
