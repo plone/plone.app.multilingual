12cb4f0e31c6ab38dc8723e3cdab796551a04d6f
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-02-09 15:21:08 +0100

    Finished acceptance test of babel view.

--- src/plone/app/multilingual/tests/test_acceptance.py
+++ src/plone/app/multilingual/tests/test_acceptance.py
@@ -1,6 +1,7 @@
 import unittest
 
 from plone.testing import layered
+from plone.app.testing import applyProfile
 # from plone.app.testing import PLONE_ZSERVER
 
 from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_ACCEPTANCE_TESTING
@@ -10,20 +11,31 @@ import robotsuite
 from zope.component.hooks import getSite
 from plone.app.multilingual.tests.utils import makeContent, makeTranslation
 from Products.CMFCore.utils import getToolByName
+from plone.dexterity.utils import createContentInContainer
+from plone.multilingual.interfaces import ILanguage
 
 import transaction
 
 
 def setUp():
     portal = getSite()
+    applyProfile(portal, 'plone.multilingualbehavior:default')
+
     language_tool = getToolByName(portal, 'portal_languages')
     language_tool.addSupportedLanguage('ca')
     language_tool.addSupportedLanguage('es')
 
-    doc1 = makeContent(portal, 'Document', id='doc1', title='EN doc')
-    doc1.setLanguage('en')
-    doc1_ca = makeTranslation(doc1, 'ca')
-    doc1_ca.edit(title="CA doc", language='ca')
+    atdoc = makeContent(portal, 'Document', id='atdoc', title='EN doc')
+    atdoc.setLanguage('en')
+    atdoc_ca = makeTranslation(atdoc, 'ca')
+    atdoc_ca.edit(title="CA doc", language='ca')
+
+    dxdoc = createContentInContainer(portal, "dxdoc", id="dxdoc", title='EN doc')
+    ILanguage(dxdoc).set_language('en')
+    dxdoc_ca = makeTranslation(dxdoc, 'ca')
+    dxdoc_ca.title = "CA doc"
+    ILanguage(dxdoc_ca).set_language('ca')
+
     transaction.commit()
 
 
