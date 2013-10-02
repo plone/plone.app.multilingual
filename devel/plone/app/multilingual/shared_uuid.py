f442bc2279b109adc4e460c7a7ea5a543b997975
Daniel Widerin <daniel@widerin.net>
2013-10-02 23:35:37 +0200

    Remove committed conflicts

--- devel/plone/app/multilingual/shared_uuid.py
+++ devel/plone/app/multilingual/shared_uuid.py
@@ -1,16 +1,12 @@
 from Acquisition import aq_base, aq_chain
-from Products.Archetypes.config import UUID_ATTR
 
 from plone.app.multilingual.content.lrf import ILanguageRootFolder
-<<<<<<< HEAD
-from plone.uuid.interfaces import IUUID, IAttributeUUID, ATTRIBUTE_NAME
-from zope.component import adapter
+from plone.uuid.interfaces import IAttributeUUID
+from plone.uuid.interfaces import ATTRIBUTE_NAME
+from plone.uuid.interfaces import IUUID
 from zope.interface import implementer
+from zope.component import adapter
 
-=======
-from plone.app.multilingual.interfaces import ISharedElement
-from plone.app.multilingual.browser.utils import is_shared
->>>>>>> Working on 2.0 merge
 
 @implementer(IUUID)
 @adapter(ILanguageRootFolder)
@@ -23,12 +19,9 @@ def lrfUUID(context):
 def attributeUUID(context):
     child = context
     for element in aq_chain(context):
-<<<<<<< HEAD
         if hasattr(child, '_v_is_shared_content') \
+           and child._v_is_shared_content \
            and ILanguageRootFolder.providedBy(element):
-=======
-        if hasattr(child, '_v_is_shared_content') and child._v_is_shared_content and ILanguageRootFolder.providedBy(element):
->>>>>>> Working on 2.0 merge
             uid = getattr(aq_base(context), ATTRIBUTE_NAME, None)
             return uid + '-' + element.id if uid is not None else None
         child = element
