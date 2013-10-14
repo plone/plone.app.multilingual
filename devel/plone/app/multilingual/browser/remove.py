0f01001f211e5bab55c4d6a45aea73c46ef8a3df
Daniel Widerin <daniel@widerin.net>
2013-10-14 14:28:34 +0200

    remove grok #2

--- devel/plone/app/multilingual/browser/remove.py
+++ devel/plone/app/multilingual/browser/remove.py
@@ -1,20 +1,15 @@
-from five import grok
-from z3c.form import button
-from plone.directives import form
+# -*- coding: utf-8 -*-
 from Acquisition import aq_parent
-from plone.multilingual.interfaces import (
-    ITranslationManager,
-    ITranslatable,
-)
+
+from plone.app.multilingual.interfaces import ITranslationManager
 from plone.app.multilingual import _
 from plone.app.multilingual.browser.interfaces import IRemoveTranslation
+from plone.directives import form
+from z3c.form import button
 
 
 class RemoveTranslationsForm(form.SchemaForm):
 
-    grok.name("remove_translations")
-    grok.context(ITranslatable)
-    grok.require("plone.app.multilingual.ManageTranslations")
     schema = form.IFormFieldProvider(IRemoveTranslation)
     ignoreContext = True
     label = _(u"label_remove_translations",
@@ -33,8 +28,9 @@ class RemoveTranslationsForm(form.SchemaForm):
         if not errors:
             for language in data['languages']:
                 manager.remove_translation(language)
-        return self.request.response.redirect(self.context.absolute_url() \
-            + '/remove_translations')
+
+        return self.request.response.redirect(
+            self.context.absolute_url() + '/remove_translations')
 
     @button.buttonAndHandler(_(u"remove selected"))
     def handle_remove(self, action):
@@ -45,5 +41,6 @@ class RemoveTranslationsForm(form.SchemaForm):
                 content = manager.get_translation(language)
                 manager.remove_translation(language)
                 aq_parent(content).manage_delObjects([content.getId()])
-        return self.request.response.redirect(self.context.absolute_url() \
-            + '/remove_translations')
+
+        return self.request.response.redirect(
+            self.context.absolute_url() + '/remove_translations')
