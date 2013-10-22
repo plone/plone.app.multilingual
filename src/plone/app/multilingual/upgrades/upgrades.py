3813663aef706d139b50bffc6bc1219bbf11c784
David Erni <david.erni@4teamwork.ch>
2013-10-22 11:54:39 +0200

    Add upgrade step for new registry value.

--- src/plone/app/multilingual/upgrades/upgrades.py
+++ src/plone/app/multilingual/upgrades/upgrades.py
@@ -1,4 +1,6 @@
 from Products.CMFCore.utils import getToolByName
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
 
 
 def reimport_css_registry(context):
@@ -10,3 +12,18 @@ def reimport_css_registry(context):
     # Refresh css
     cssregistry = getToolByName(context, 'portal_css')
     cssregistry.cookResources()
+
+
+def upgrade_2_to_3_add_missing_registry_entry(context):
+    registry = getUtility(IRegistry)
+
+    # don't re-create if it already exists
+    key = ('plone.app.multilingual.interfaces.'
+           'IMultiLanguageExtraOptionsSchema.'
+           'bypass_languageindependent_field_permission_check')
+    if key in registry:
+        return
+
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile(
+        'profile-plone.app.multilingual.upgrades:3')
