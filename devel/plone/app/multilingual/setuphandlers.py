3f1eba1b9253f27386596bfc16bdf3ccd565829c
Steven Looman <steven.looman@gmail.com>
2013-11-11 16:16:01 +0100

    Added uninstall profile. Refs #92

---
+++ devel/plone/app/multilingual/setuphandlers.py
@@ -0,0 +1,33 @@
+from plone.dexterity.interfaces import IDexterityFTI
+from Products.CMFCore.utils import getToolByName
+
+
+def removeBehaviors(context):
+    """Remove p.a.multilingual behaviors from p.a.contenttypes types."""
+
+    if context.readDataFile('plone.app.multilingual_uninstall.txt') is None:
+        return
+
+    portal = context.getSite()
+    portal_types = getToolByName(portal, 'portal_types')
+
+    behavior = 'plone.app.multilingual.dx.interfaces.IDexterityTranslatable'
+    # plone.app.contenttype types
+    typeNames = [
+        'Document',
+        'File',
+        'Folder',
+        'Image',
+        'Link',
+        'News Item',
+    ]
+    for name in typeNames:
+        type_ = portal_types.get(name)
+
+        # safety first
+        if not type_ or not IDexterityFTI.providedBy(type_):
+            continue
+
+        behaviors = list(type_.behaviors)
+        behaviors.remove(behavior)
+        type_.behaviors = tuple(behaviors)
