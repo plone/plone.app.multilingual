ebee64ea39a7834c629820ad86206f186fd171f9
Steven Looman <steven.looman@gmail.com>
2013-11-12 19:12:58 +0100

    Created test (and fix) for directives. #94

--- devel/plone/app/multilingual/dx/directives.py
+++ devel/plone/app/multilingual/dx/directives.py
@@ -1,4 +1,6 @@
 from plone.supermodel.directives import MetadataListDirective
+from zope.interface import Interface
+from zope.interface.interfaces import IInterface
 
 
 LANGUAGE_INDEPENDENT_KEY = u'plone.app.multilingual.languageindependent'
@@ -12,6 +14,20 @@ class languageindependent(MetadataListDirective):
 
     key = LANGUAGE_INDEPENDENT_KEY
     value = 'true'
+
+    def factory(self, *args):
+        """The languageindependent directive accepts as arguments one or more
+        fieldnames (string) of fields which should be searchable.
+        """
+        if not args:
+            raise TypeError('The languageindependent directive expects at '
+                            'least one argument.')
+
+        form_interface = Interface
+        if IInterface.providedBy(args[0]):
+            form_interface = args[0]
+            args = args[1:]
+        return [(form_interface, field, self.value) for field in args]
 
 
 __all__ = ('languageindependent',)
