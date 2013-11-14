ba48bc6b8cec40fb0b02e633c1361aa51a2dcfbd
Asko Soukka <asko.soukka@iki.fi>
2013-11-14 03:53:20 +0100

    Fix RelationList-support to support p.a.contenttypes

--- devel/plone/app/multilingual/dx/utils.py
+++ devel/plone/app/multilingual/dx/utils.py
@@ -8,7 +8,7 @@ from plone.dexterity.interfaces import IDexterityFTI
 from plone.app.multilingual.interfaces import ILanguageIndependentFieldsManager
 
 from plone.app.multilingual.dx.interfaces import ILanguageIndependentField
-from z3c.relationfield.interfaces import IRelationValue
+from z3c.relationfield.interfaces import IRelationValue, IRelationList
 
 from plone.app.multilingual.interfaces import ILanguage
 from zope.component import queryAdapter
@@ -17,6 +17,7 @@ from plone.app.multilingual.interfaces import ITranslationManager
 from zope.app.intid.interfaces import IIntIds
 from zope import component
 from z3c.relationfield import RelationValue
+from plone.dexterity.utils import iterSchemata
 
 _marker = object()
 
@@ -28,53 +29,42 @@ class LanguageIndependentFieldsManager(object):
         self.context = context
 
     def has_independent_fields(self):
-        fti = getUtility(IDexterityFTI, name=self.context.portal_type)
-        schemas = []
-        schemas.append(fti.lookupSchema())
-
-        for behavior_schema in \
-                utils.getAdditionalSchemata(self.context,
-                                            self.context.portal_type):
-            if behavior_schema is not None:
-                schemas.append(behavior_schema)
-
-        for schema in schemas:
+        for schema in iterSchemata(self.context):
             for field_name in schema:
                 if ILanguageIndependentField.providedBy(schema[field_name]):
                     return True
         return False
 
+    def copy_relation(self, relation_value, target_language):
+        obj = relation_value.to_object
+        intids = component.getUtility(IIntIds)
+        translation = ITranslationManager(obj).get_translation(target_language)
+        if translation:
+            return RelationValue(intids.getId(translation))
+        else:
+            return RelationValue(intids.getId(obj))
+
     def copy_fields(self, translation):
-
-        fti = getUtility(IDexterityFTI, name=self.context.portal_type)
-        schemas = []
-        schemas.append(fti.lookupSchema())
-
-        for behavior_schema in \
-                utils.getAdditionalSchemata(self.context,
-                                            self.context.portal_type):
-            if behavior_schema is not None:
-                schemas.append(behavior_schema)
-
         doomed = False
-        for schema in schemas:
+
+        target_language = queryAdapter(translation, ILanguage).get_language()
+        relation_copier =\
+            lambda r, l=target_language, f=self.copy_relation: f(r, l)
+
+        for schema in iterSchemata(self.context):
             for field_name in schema:
                 if ILanguageIndependentField.providedBy(schema[field_name]):
-                    doomed = True
-
                     value = getattr(schema(self.context), field_name, _marker)
-                    if IRelationValue.providedBy(value):
-                        obj = value.to_object
-                        adapter = queryAdapter(translation, ILanguage)
-                        trans_obj = ITranslationManager(obj)\
-                            .get_translation(adapter.get_language())
-                        if trans_obj:
-                            intids = component.getUtility(IIntIds)
-                            value = RelationValue(intids.getId(trans_obj))
-                    if not (value == _marker):
-                        # We check if not (value == _marker) because
-                        # z3c.relationfield has an __eq__
-                        setattr(schema(translation), field_name, value)
+
+                    if value == _marker:
+                        continue
+                    elif IRelationValue.providedBy(value):
+                        value = self.copy_relation(value, target_language)
+                    elif IRelationList.providedBy(schema[field_name]):
+                        value = map(relation_copier, value or [])
+
+                    doomed = True
+                    setattr(schema(translation), field_name, value)
 
         # If at least one field has been copied over to the translation
         # we need to inform subscriber to trigger an ObjectModifiedEvent
