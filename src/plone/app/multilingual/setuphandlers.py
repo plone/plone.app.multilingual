30d6683602e2c6321e4c91df98717115dda66666
Asko Soukka <asko.soukka@iki.fi>
2014-10-05 16:48:45 +0300

    Fix to enable behaviors in setuphandlers; Add uninstall hook; Cleanup

--- src/plone/app/multilingual/setuphandlers.py
+++ src/plone/app/multilingual/setuphandlers.py
@@ -1,50 +1,73 @@
 # -*- coding: utf-8 -*-
-from Products.CMFCore.utils import getToolByName
+import pkg_resources
 from Products.CMFPlone.interfaces import INonInstallable
-from plone.dexterity.interfaces import IDexterityFTI
 from zope.interface import implementer
+
+try:
+    pkg_resources.get_distribution('plone.app.contenttypes')
+except pkg_resources.DistributionNotFound:
+    HAS_PLONE_APP_CONTENTTYPES = False
+else:
+    HAS_PLONE_APP_CONTENTTYPES = True
 
 
 @implementer(INonInstallable)
 class HiddenProfiles(object):
 
     def getNonInstallableProfiles(self):
-        """
-        Prevents uninstall profile from showing up in the profile list
+        """Prevents uninstall profile from showing up in the profile list
         when creating a Plone site.
+
         """
         return [
-            # u'plone.app.multilingual:default',  # XXX: TODO: this one too?
             u'plone.app.multilingual:uninstall',
         ]
 
 
-def removeBehaviors(context):
-    """Remove p.a.multilingual behaviors from p.a.contenttypes types."""
+def step_default_various(context):
+    if context.readDataFile('plone.app.multilingual_default.txt') is None:
+        return
+    portal = context.getSite()
+    enable_translatable_behavior(portal)
 
+
+def enable_translatable_behavior(portal):
+    types_tool = portal.portal_types
+
+    # Iterate through all Dexterity content type
+    all_ftis = types_tool.listTypeInfo()
+    dx_ftis = [x for x in all_ftis if getattr(x, 'behaviors', False)]
+    for fti in dx_ftis:
+
+        # Enable translatable behavior for all types
+        behaviors = [i for i in fti.behaviors]
+        behaviors.extend([
+            'plone.app.multilingual.dx.interfaces.IDexterityTranslatable',
+        ])
+        behaviors = tuple(set(behaviors))
+        fti._updateProperty('behaviors', behaviors)
+
+
+def step_uninstall_various(context):
     if context.readDataFile('plone.app.multilingual_uninstall.txt') is None:
         return
-
     portal = context.getSite()
-    portal_types = getToolByName(portal, 'portal_types')
+    disable_translatable_behavior(portal)
 
-    behavior = 'plone.app.multilingual.dx.interfaces.IDexterityTranslatable'
-    # plone.app.contenttype types
-    typeNames = [
-        'Document',
-        'File',
-        'Folder',
-        'Image',
-        'Link',
-        'News Item',
-    ]
-    for name in typeNames:
-        type_ = portal_types.get(name)
 
-        # safety first
-        if not type_ or not IDexterityFTI.providedBy(type_):
-            continue
+def disable_translatable_behavior(portal):
+    """Remove p.a.multilingual behaviors from p.a.contenttypes types
+    """
+    types_tool = portal.portal_types
 
-        behaviors = list(type_.behaviors)
-        behaviors.remove(behavior)
-        type_.behaviors = tuple(behaviors)
+    # Iterate through all Dexterity content type
+    all_ftis = types_tool.listTypeInfo()
+    dx_ftis = [x for x in all_ftis if getattr(x, 'behaviors', False)]
+    for fti in dx_ftis:
+
+        # Disable translatable behavior from all types
+        behaviors = [
+            i for i in fti.behaviors if i !=
+            'plone.app.multilingual.dx.interfaces.IDexterityTranslatable'
+        ]
+        fti._updateProperty('behaviors', behaviors)
