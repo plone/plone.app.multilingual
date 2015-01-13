3158c2095726ba7fb1bfe16fb4fe1a81be4945f9
Jens W. Klein <jk@kleinundpartner.at>
2015-01-13 10:44:38 +0100

    fix: special template and validator for language independen fields in add form did not get grip on fieldsets (groups)

--- src/plone/app/multilingual/dx/interfaces.py
+++ src/plone/app/multilingual/dx/interfaces.py
@@ -11,3 +11,14 @@ class IDexterityTranslatable(ITranslatable):
 
 class ILanguageIndependentField(Interface):
     """ Marker interface for language independent fields """
+
+
+class IMultilingualAddForm(Interface):
+    """Marker Interface for multilingual add forms
+
+    This marker is intended to be provided by the main multilingual add form
+    ans to be provided by the groups (aka fieldset) forms
+    (plone.z3cform.fieldsets.group.Group). This is neccessary in order to
+    make the special template renderer for ILanguageIndependentField fields
+    work in fieldset tabs too.
+    """
