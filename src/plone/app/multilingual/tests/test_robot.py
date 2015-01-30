a7514e250a788e16e9c385a27045682a0f5102c4
Benoît Suttor <benoit.suttor@imio.be>
2015-01-30 14:55:25 +0100

    clean test_robot.py

--- src/plone/app/multilingual/tests/test_robot.py
+++ src/plone/app/multilingual/tests/test_robot.py
@@ -1,23 +1,15 @@
-from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_ROBOT_TESTING
+# -*- coding: utf-8 -*-
 from plone.testing import layered
+from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_ROBOT_TESTING
 
-import os
 import robotsuite
 import unittest
 
 
 def test_suite():
     suite = unittest.TestSuite()
-    current_dir = os.path.abspath(os.path.dirname(__file__))
-    robot_dir = os.path.join(current_dir, 'robot')
-    robot_tests = [
-        os.path.join('robot', doc) for doc in
-        os.listdir(robot_dir) if doc.endswith('.robot') and
-        doc.startswith('test_')
-    ]
-    for test in robot_tests:
-        suite.addTests([
-            layered(robotsuite.RobotTestSuite(test),
-                    layer=PLONEAPPMULTILINGUAL_ROBOT_TESTING),
-        ])
+    suite.addTests([
+        layered(robotsuite.RobotTestSuite('robot'),
+                layer=PLONEAPPMULTILINGUAL_ROBOT_TESTING),
+    ])
     return suite
