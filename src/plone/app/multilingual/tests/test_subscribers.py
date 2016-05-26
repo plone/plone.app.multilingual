468b4055e94b12050448e9b760533f2ec8e26242
Daniel Jowett <daniel@jowettenterprises.com>
2016-05-26 17:50:38 +0100

    Add tests for copying / moving content to Media folders (proving issue #205)

--- src/plone/app/multilingual/tests/test_subscribers.py
+++ src/plone/app/multilingual/tests/test_subscribers.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import unittest2 as unittest
 
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.interfaces import ILanguage
+from plone.app.multilingual.interfaces import IPloneAppMultilingualInstalled
 from plone.app.multilingual.testing import PAM_FUNCTIONAL_TESTING
 from plone.dexterity.utils import createContentInContainer
-from plone.app.multilingual.interfaces import IPloneAppMultilingualInstalled
 from zope.interface import alsoProvides
 
 
@@ -63,3 +64,90 @@ class TestSubscribers(unittest.TestCase):
         self.portal['en'].manage_pasteObjects(id_)
         a_ca_copied = self.portal['en'][a_ca.id]
         self.assertEqual(ILanguage(a_ca_copied).get_language(), 'en')
+
+
+    def test_moved_to_media_folder(self):
+        """When an object is moved from within one Language Root Folder into
+        the Language Independent Folder (named 'Media') it becomes language 
+        independent, and it should be visible from the media folder accessed
+        from within other Language Root Folders
+        """
+        a_ca = createContentInContainer(
+            self.portal['ca'], 'Document', title=u"Test document")
+
+        # Test a paste into a subfolder to be ultra cautious
+        ca_media_subfolder = createContentInContainer(
+            self.portal['ca']['media'], 'Folder', title=u"A Folder")
+
+        subfolder_name = ca_media_subfolder.id
+
+        id_ = self.portal['ca'].manage_cutObjects(a_ca.id)
+        ca_media_subfolder.manage_pasteObjects(id_)
+
+        # Get both media folders afresh
+        ca_media_subfolder = self.portal['ca']['media'][subfolder_name]
+        en_media_subfolder = self.portal['en']['media'][subfolder_name]
+        
+        # Check it is in both folder listings
+        self.assertTrue(a_ca.id in ca_media_subfolder)
+        self.assertTrue(a_ca.id in en_media_subfolder)
+
+        # Check it is language independent
+        copy_in_en = en_media_subfolder[a_ca.id]
+        self.assertEqual(ILanguage(copy_in_en).get_language(), '')
+        copy_in_ca = ca_media_subfolder[a_ca.id]
+        self.assertEqual(ILanguage(copy_in_ca).get_language(), '')
+
+        # Check it is returned in catalog search
+        catalog = getToolByName(self.portal, 'portal_catalog')
+
+        ca_subfolder_path = '/'.join(ca_media_subfolder.getPhysicalPath())
+        ca_folder_contents = [r.id for r in catalog(path=ca_subfolder_path)]
+        self.assertTrue(a_ca.id in ca_folder_contents)
+
+        en_subfolder_path = '/'.join(en_media_subfolder.getPhysicalPath())
+        en_folder_contents = [r.id for r in catalog(path=en_subfolder_path)]
+        self.assertTrue(a_ca.id in en_folder_contents)
+
+
+    def test_copied_to_media_folder(self):
+        """When an object is copied from within one Language Root Folder into
+        the Language Independent Folder (named 'Media') it becomes language 
+        independent, and it should be visible from the media folder accessed
+        from within other Language Root Folders
+        """
+        a_ca = createContentInContainer(
+            self.portal['ca'], 'Document', title=u"Test document")
+
+        # Test a paste into a subfolder to be ultra cautious
+        ca_media_subfolder = createContentInContainer(
+            self.portal['ca']['media'], 'Folder', title=u"A Folder")
+
+        subfolder_name = ca_media_subfolder.id
+        id_ = self.portal['ca'].manage_copyObjects(a_ca.id)
+        ca_media_subfolder.manage_pasteObjects(id_)
+
+        # Get both media folders afresh
+        ca_media_subfolder = self.portal['ca']['media'][subfolder_name]
+        en_media_subfolder = self.portal['en']['media'][subfolder_name]
+        
+        # Check it is in both folder listings
+        self.assertTrue(a_ca.id in ca_media_subfolder)
+        self.assertTrue(a_ca.id in en_media_subfolder)
+
+        # Check it is language independent
+        copy_in_en = en_media_subfolder[a_ca.id]
+        self.assertEqual(ILanguage(copy_in_en).get_language(), '')
+        copy_in_ca = ca_media_subfolder[a_ca.id]
+        self.assertEqual(ILanguage(copy_in_ca).get_language(), '')
+
+        # Check it is returned in catalog search
+        catalog = getToolByName(self.portal, 'portal_catalog')
+
+        ca_subfolder_path = '/'.join(ca_media_subfolder.getPhysicalPath())
+        ca_folder_contents = [r.id for r in catalog(path=ca_subfolder_path)]
+        self.assertTrue(a_ca.id in ca_folder_contents)
+
+        en_subfolder_path = '/'.join(en_media_subfolder.getPhysicalPath())
+        en_folder_contents = [r.id for r in catalog(path=en_subfolder_path)]
+        self.assertTrue(a_ca.id in en_folder_contents)
