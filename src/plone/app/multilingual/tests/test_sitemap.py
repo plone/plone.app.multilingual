7ebc52a49ab1c9a9214ccf50b5c0a201ce3d743f
Mikel Larreategi <mlarreategi@codesyntax.com>
2016-06-03 10:39:44 +0200

    adapt the tests for PAM 1.x

--- src/plone/app/multilingual/tests/test_sitemap.py
+++ src/plone/app/multilingual/tests/test_sitemap.py
@@ -2,38 +2,55 @@
 from StringIO import StringIO
 from Products.CMFCore.utils import getToolByName
 from gzip import GzipFile
-from plone.app.multilingual.testing import PAM_FUNCTIONAL_TESTING
-from plone.dexterity.utils import createContentInContainer
+from plone.app.multilingual.testing import PLONEAPPMULTILINGUAL_FUNCTIONAL_TESTING
 from zope.component import getMultiAdapter
+from .utils import makeContent
+from .utils import makeTranslation
+from zope.interface import alsoProvides
+from plone.app.layout.navigation.interfaces import INavigationRoot
 
 import unittest
 
 # This class largely inspired by plone/app/layout/sitemap/tests/test_sitemap.py
 class TestSitemap(unittest.TestCase):
-    layer = PAM_FUNCTIONAL_TESTING
+    layer = PLONEAPPMULTILINGUAL_FUNCTIONAL_TESTING
 
     def setUp(self):
         self.portal = self.layer['portal']
         self.request = self.layer['request']
-
+        self.portal.portal_languages.addSupportedLanguage('ca')
+        self.portal.portal_languages.addSupportedLanguage('es')
+        en = makeContent(self.portal, 'Folder', id='en')
+        en.setLanguage('en')
+        ca = makeTranslation(en, 'ca')
+        ca.setId('ca')
+        alsoProvides(ca, INavigationRoot)
+        es = makeTranslation(en, 'es')
+        es.setId('es')
+        alsoProvides(es, INavigationRoot)
         self.sitemap = getMultiAdapter((self.portal, self.portal.REQUEST),
                                        name='sitemap.xml.gz')
         self.site_properties = getToolByName(
             self.portal, 'portal_properties').site_properties
         self.site_properties.manage_changeProperties(enable_sitemap=True)
-
-        createContentInContainer(
-            self.portal['en']['media'], 'Document', title=u"Test document")
-        # ^ This will be shadowed to all language independent folders
-
-        createContentInContainer(
-            self.portal['ca'], 'Document', title=u"Test document")
-
-        createContentInContainer(
-            self.portal['es'], 'Document', title=u"Test document")
-
-        createContentInContainer(
-            self.portal['en'], 'Document', title=u"Test document")
+        docca = makeContent(
+            self.portal['ca'],
+            'Document',
+            title=u'Test document',
+            id='test-document')
+        doces = makeContent(
+            self.portal['es'],
+            'Document',
+            title=u'Test document',
+            id='test-document')
+        docen = makeContent(
+            self.portal['en'],
+            'Document',
+            title=u'Test document',
+            id='test-document')
+        docca.setLanguage('ca')
+        doces.setLanguage('es')
+        docen.setLanguage('en')
 
     def uncompress(self, sitemapdata):
         sio = StringIO(sitemapdata)
@@ -46,28 +63,24 @@ class TestSitemap(unittest.TestCase):
         '''
         Requests for the sitemap on portalroot return all languages
         '''
-
         xml = self.uncompress(self.sitemap())
-        self.assertTrue('<loc>http://nohost/plone/ca/test-document</loc>' in xml)
-        self.assertTrue('<loc>http://nohost/plone/en/test-document</loc>' in xml)
-        self.assertTrue('<loc>http://nohost/plone/es/test-document</loc>' in xml)
-
-        self.assertTrue('<loc>http://nohost/plone/ca/media/test-document</loc>' in xml)
-        self.assertTrue('<loc>http://nohost/plone/en/media/test-document</loc>' in xml)
-        self.assertTrue('<loc>http://nohost/plone/es/media/test-document</loc>' in xml)
-
+        self.assertTrue(
+            '<loc>http://nohost/plone/ca/test-document</loc>' in xml)
+        self.assertTrue(
+            '<loc>http://nohost/plone/en/test-document</loc>' in xml)
+        self.assertTrue(
+            '<loc>http://nohost/plone/es/test-document</loc>' in xml)
 
     def test_navroot_sitemap(self):
         '''
         Sitemap generated from a LanguageRootFolder (an INavigationRoot)
         '''
-        sitemap = getMultiAdapter((self.portal.es, self.portal.REQUEST),
-                                       name='sitemap.xml.gz')
+        sitemap = getMultiAdapter(
+            (self.portal.es, self.portal.REQUEST), name='sitemap.xml.gz')
         xml = self.uncompress(sitemap())
-        self.assertFalse('<loc>http://nohost/plone/ca/test-document</loc>' in xml)
-        self.assertFalse('<loc>http://nohost/plone/en/test-document</loc>' in xml)
-        self.assertTrue('<loc>http://nohost/plone/es/test-document</loc>' in xml)
-
-        self.assertFalse('<loc>http://nohost/plone/ca/media/test-document</loc>' in xml)
-        self.assertFalse('<loc>http://nohost/plone/en/media/test-document</loc>' in xml)
-        self.assertTrue('<loc>http://nohost/plone/es/media/test-document</loc>' in xml)
+        self.assertFalse(
+            '<loc>http://nohost/plone/ca/test-document</loc>' in xml)
+        self.assertFalse(
+            '<loc>http://nohost/plone/en/test-document</loc>' in xml)
+        self.assertTrue(
+            '<loc>http://nohost/plone/es/test-document</loc>' in xml)
