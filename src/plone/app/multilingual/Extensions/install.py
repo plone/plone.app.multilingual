5f8f011232af8079177c4ca24a098c2ba44a81b0
Maurits van Rees <maurits@vanrees.org>
2018-01-30 23:51:41 +0100

    Removed Extensions/Install.py.
    This was only there as wrapper for applying our uninstall profile, but that wrapper is no longer needed.
    I think around Plone 4.3.9 this was added to CMFQuickInstaller,
    and since Plone 5.1 the CMFQuickInstaller is not even really used anymore.

--- src/plone/app/multilingual/Extensions/install.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Legacy install/uninstall-methods to guard from re-installing/uninstalling"""
-from Products.CMFCore.utils import getToolByName
-
-
-def uninstall(context, reinstall=False):
-    setup_tool = getToolByName(context, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile(
-        'profile-plone.app.multilingual:uninstall', purge_old=False)
