b5d0d5c048ae623246c280351d28a81c85e192ad
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-01-08 09:28:44 +0100

    only translate non-empty values

--- src/plone/app/multilingual/browser/translate.py
+++ src/plone/app/multilingual/browser/translate.py
@@ -15,37 +15,40 @@ import json
 def translate_text(original_text, source_language, target_language, service=None):
     """translate the text"""
 
-    if service is not None:
-        # if an specific adapter is requested, use it if available
+    if original_text:
+        # Initial shortcut: translate only non-empty values
 
-        adapter = getUtility(IExternalTranslationService, name=service)
-        if not adapter.is_available():
-            return None
+        if service is not None:
+            # if an specific adapter is requested, use it if available
 
-        adapters = [adapter]
+            adapter = getUtility(IExternalTranslationService, name=service)
+            if not adapter.is_available():
+                return None
 
-    else:
+            adapters = [adapter]
 
-        adapters = [
-            adapter
-            for _, adapter in getUtilitiesFor(IExternalTranslationService)
-            if adapter.is_available()
-        ]
+        else:
+            # Get all available adapters
+            adapters = [
+                adapter
+                for _, adapter in getUtilitiesFor(IExternalTranslationService)
+                if adapter.is_available()
+            ]
 
-    sorted_adapters = sorted(adapters, key=lambda x: x.order)
+        sorted_adapters = sorted(adapters, key=lambda x: x.order)
 
-    for adapter in sorted_adapters:
-        available_languages = adapter.available_languages()
-        if (
-            not available_languages
-            or (source_language, target_language) in available_languages
-        ):
-            translation = adapter.translate_content(
-                original_text, source_language, target_language
-            )
+        for adapter in sorted_adapters:
+            available_languages = adapter.available_languages()
+            if (
+                not available_languages
+                or (source_language, target_language) in available_languages
+            ):
+                translation = adapter.translate_content(
+                    original_text, source_language, target_language
+                )
 
-            if translation:
-                return translation
+                if translation:
+                    return translation
 
     return None
 
