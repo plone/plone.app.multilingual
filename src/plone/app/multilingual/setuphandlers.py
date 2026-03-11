0e589ef8c9cd420f7174d465f81aa96e60b2a129
David Glick <david@glicksoftware.com>
2026-03-11 11:47:42 -0700

    Always restore document_view

--- src/plone/app/multilingual/setuphandlers.py
+++ src/plone/app/multilingual/setuphandlers.py
@@ -2,11 +2,9 @@ from plone.app.multilingual import logger
 from plone.app.multilingual.browser.setup import SetupMultilingualSite
 from plone.app.multilingual.itg import addAttributeTG
 from plone.base.interfaces import INonInstallable
-from plone.volto.interfaces import IPloneVoltoCoreLayer
 from Products.CMFCore.utils import getToolByName
 from zope.component.hooks import getSite
 from zope.interface import implementer
-from zope.globalrequest import getRequest
 
 
 @implementer(INonInstallable)
@@ -84,10 +82,6 @@ def disable_language_switcher(portal):
         method for method in site.view_methods if method != "language-switcher"
     )
     if site.default_view == "language-switcher":
-        request = getRequest()
-        if IPloneVoltoCoreLayer.providedBy(request):
-            site.default_view = "document_view"
-        else:
-            site.default_view = "listing_view"
+        site.default_view = "document_view"
 
     logger.info("Language switcher disabled")
